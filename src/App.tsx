785bde3bb556ede06364f78b3e1c5fcdf5f2e90d
lincoln Duarte <lincolnskyrim_99@hotmail.com>
2023-05-08 20:37:51 -0300

    chore: add axios

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,12 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect } from 'react'
+import axios from 'axios'
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  useEffect(() => {
+    axios.get('http://localhost:3000/user').then(console.log)
+  }, [])
+
+  return <div className="App">Hello World</div>
 }
 
-export default App;
+export default App
